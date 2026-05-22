b5fbb522ff914788d62988ba297c325ceb5f763a
roshhh <mjroshan36@gmail.com>
2026-05-22 16:38:44 +0530

    feat: add S3 public access block scanner

---
+++ scanners/s3_scanner.py
@@ -0,0 +1,86 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def list_buckets():
+    """
+    Connect to AWS and return all s3 buckets in the account.
+    """
+    s3_client = boto3.client('s3')
+
+    response = s3_client.list_buckets()
+
+    return response['Buckets']
+
+def check_public_access_block(bucket_name):
+    """
+    Check if Block Public Access is properly enabled on a bucket.
+    Returns a finding if any setting is disabled.
+    """
+    s3_client = boto3.client('s3')
+
+    try:
+        response = s3_client.get_public_access_block(
+            Bucket=bucket_name
+            )
+        config = response['PublicAccessBlockConfiguration']
+
+        issues = []
+
+        if not config.get('BlockPublicAcls'):
+            issues.append('BlockPublicAcls is disabled')
+
+        if not config.get('IgnorePublicAcls'):
+            issues.append('IgnorePublicAcls is disabled')
+
+        if not config.get('BlockPublicPolicy'):
+            issues.append('BlockPublicPolicy is disabled')
+
+        if not config.get('RestrictPublicBuckets'):
+            issues.append('RestrictPublicBuckets is disabled')
+
+        if issues:
+            return {
+                'bucket': bucket_name,
+                'status': 'FAIL',
+                'issues': issues
+            }                
+        else:
+            return {
+                'bucket': bucket_name,
+                'status': 'PASS',
+                'issues' : []
+            }
+    except ClientError as e:
+        error_code = e.response['Error']['Code']
+
+        if error_code == 'NoSuckPublicAccessBlockConfiguration':
+            return {
+                'bucket': bucket_name,
+                'status': 'FAIL',
+                'issues': ['No Block Public Access configuration found']
+            }
+        else:
+            return {
+                'bucket': bucket_name,
+                'status': 'Error',
+                'issues': [f'Unexpected error: {error_code}']
+            }
+        
+
+if __name__ == "__main__":
+    buckets = list_buckets()
+
+    if not buckets:
+        print ("No S3 buckets found in the account.")
+    else:
+        print(f"Scanning {len(buckets)} bucket(s)... \n")
+        for bucket in buckets:
+            result = check_public_access_block(bucket['Name'])
+            print(f"Bucket: {result['bucket']}")
+            print(f"Status: {result['status']}")
+            if result['issues']:
+                for issue in result['issues']:
+                    print(f" ! {issue}")
+            print() 
+
+        
